e46859e1fe6e5e925d548c491640929fb357e679
meghana786 <160600002+meghana786@users.noreply.github.com>
2024-03-14 15:43:44 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -21,30 +21,38 @@ const PLAYERS = [
     "Slingo"
 ];
 
-// initialize players with image and strength
+// Initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = '';
-
     // Instead of forloop use Map method
     // Code here
-
-    return detailedPlayers;
+    return players.map((player, index) => ({
+        name: player,
+        strength: getRandomStrength(),
+        image: `images/super-${index + 1}.png`,
+        type: index % 2 === 0 ? 'hero' : 'villain' 
+    }));
 }
 
-// getting random strength
+// Getting random strength
 const getRandomStrength = () => {
     return Math.ceil(Math.random() * 100);
 }
 
 // Build player template
 const buildPlayers = (players, type) => {
-    let fragment = '';
-
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
-
-    return fragment;
+    return players
+        .filter(player => player.type === type)
+        .map(player => `
+            <div class="player">
+                <img src="${player.image}">
+                <div class="name">${player.name}</div>
+                <div class="strength">${player.strength}</div>
+            </div>
+        `)
+        .join('');
 }
 
 // Display players in HTML
@@ -52,7 +60,6 @@ const viewPlayers = (players) => {
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 }
-
 
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
